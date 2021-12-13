0d361b6c87247aafedbd56ea8d59d1a72b6ae11d
Mystudentunity <george.calin@s.unibuc.ro>
2021-12-13 21:35:08 +0200

    Update PlayerHandController.cs

--- Assets/Scripts/Player-Controller/PlayerHandController.cs
+++ Assets/Scripts/Player-Controller/PlayerHandController.cs
@@ -6,11 +6,15 @@ namespace TheDarkPath
 {
     public class PlayerHandController : MonoBehaviour
     {
-        //private float rotationAngle = 0f;
-        //private float rotationSpeed = 10.0f;
         [SerializeField]
-        private GameObject player = null;
+        private GameObject player;
+        public new Camera camera;
 
+        private void Start()
+        {
+            if (camera == null)
+                Debug.LogError("camera is missing");
+        }
         private void FixedUpdate()
         {
             Rotate();
@@ -18,16 +22,15 @@ namespace TheDarkPath
 
         void Rotate()
         {
-            //Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            //rotationAngle = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
-            //Quaternion rotation = Quaternion.AngleAxis(rotationAngle, Vector3.forward);
-            //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
-
-            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            difference.Normalize();
+            Vector2 difference = (camera.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
             float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
 
+            RotateIf(rotationZ);
+        }
+
+        void RotateIf(float rotationZ)
+        {
             if (rotationZ < -90f || rotationZ > 90f)
             {
                 if (player.transform.eulerAngles.y == 0)
